8e73635724db2c118a79a5c412d5048a467a7461
Benson <bensonlag200@gmail.com>
2024-12-01 12:40:35 -0500

    Delete src directory

--- src/main/java/Inso/Examen/infra/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package Inso.Examen.infra.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
