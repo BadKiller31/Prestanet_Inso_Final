ef5667fe058af5d11439c74448f0fff822cf2652
Bren pc <brayanp.cabrera@gmail.com>
2024-12-02 13:58:30 -0500

    changues: filtro cors

--- src/main/java/Inso/Examen/infra/security/WebConfig.java
+++ src/main/java/Inso/Examen/infra/security/WebConfig.java
@@ -20,23 +20,27 @@ public class WebConfig implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest,
-                         ServletResponse servletResponse,
-                         FilterChain filterChain) throws IOException, ServletException {
+            ServletResponse servletResponse,
+            FilterChain filterChain) throws IOException, ServletException {
 
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         HttpServletRequest request = (HttpServletRequest) servletRequest;
 
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        // Configurar varios orígenes permitidos
+        String origin = request.getHeader("Origin");
+        if ("http://localhost:4200".equals(origin) || "https://prestanet-inso-final.onrender.com".equals(origin)) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+        }
+
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, HEAD, PATCH");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, credential, X-XSRF-TOKEN");
+        response.setHeader("Access-Control-Allow-Headers",
+                "Content-Type, Authorization, X-Requested-With, credential, X-XSRF-TOKEN");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
             filterChain.doFilter(servletRequest, servletResponse);
         }
-
     }
 
     @Override
